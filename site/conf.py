1bd0ebdf46a59c0ccedcb44dba3c97f86560d560
Nina Di Cara <ninadicara@gmail.com>
2021-12-03 15:08:45 +0000

    Add mailing list sign-up page

--- site/conf.py
+++ site/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Data Ethics Club'
-copyright = '2021, Natalie Thurlby and Nina Di Cara'
-author = 'Natalie Thurlby and Nina Di Cara'
+project = "Data Ethics Club"
+copyright = "2021, Natalie Thurlby and Nina Di Cara"
+author = "Natalie Thurlby and Nina Di Cara"
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.1.0'
+release = "v0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,21 +30,21 @@ release = 'v0.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_nb',
-
+extensions = [
+    "myst_nb",
 ]
 
 myst_enable_extensions = [
-  "colon_fence",
+    "colon_fence",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -52,28 +52,28 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'pydata_sphinx_theme'
+html_theme = "pydata_sphinx_theme"
 
 html_theme_options = {
-    'github_url': 'https://github.com/very-good-science/data-ethics-club',
-    'twitter_url': 'https://twitter.com/hashtag/DataEthicsClub',
-    'search_bar_text': 'Search this site...',
-    'show_prev_next': True,
-    'footer_items': ['license-footer', 'sphinx-version'],
+    "github_url": "https://github.com/very-good-science/data-ethics-club",
+    "twitter_url": "https://twitter.com/hashtag/DataEthicsClub",
+    "search_bar_text": "Search this site...",
+    "show_prev_next": True,
+    "footer_items": ["license-footer", "sphinx-version"],
 }
 
 html_sidebars = {
-    'index': [],
-    'contents/reading-list': [],
-    'contents/contact':[],
-    'contents/get-involved':[],
-    'contents/administration': [],
-
+    "index": [],
+    "contents/reading-list": [],
+    "contents/contact": [],
+    "contents/get-involved": [],
+    "contents/administration": [],
+    "contents/mailing-list": [],
 }
 
-html_favicon = '_static/favicon.png'
+html_favicon = "_static/favicon.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
